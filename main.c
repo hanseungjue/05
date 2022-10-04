551e5936b8eb5df5871fe88adc0280029a765dab
hanseungjue <hansj0628@naver.com>
2022-10-04 17:56:57 +0900

    task5

--- main.c
+++ main.c
@@ -4,22 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a, b;
-	char i;
-	int re;
+	int answer=59;
+	int x;
+	int a=0;
 	
-	printf("enter the calculation:");
-	scanf("%d%c%d",&a,&i,&b);
-	
-	if(i=='+')
-	   re=a+b;
-	else if(i=='-')
-	   re=a-b;
-	else if(i=='*')
-	   re=a*b;
-	else
-	   re=a/b;
-	
-	printf("=%i",re);
+	do
+	{
+		printf("숫자를 입력하시오:");
+		scanf("%d",&x);
+		if(x<answer)
+		    printf("lower\n");
+		else if(x>answer)
+		    printf("higher\n");
+		a++;
+	}
+	while(x!=answer);
+	printf("축하!\n");
+	printf("시도횟수:%d",a);
     return 0;  
 }
